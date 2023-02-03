da964a46f100562300120a229205b13770aecadb
Derek Hearst <derekallenhearst@gmail.com>
2023-02-02 18:31:11 -0700

    maybe fix process thing

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client/edge'
 export const prisma = new PrismaClient({
 	datasources: {
 		db: {
-			url: process.env.NEW_DATABASE_URL
+			url: env.NEW_DATABASE_URL
 		}
 	}
 })
