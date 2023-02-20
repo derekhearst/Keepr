02a0bc2b579c17f9fee2ae4b147296ebe3a96ad4
Derek Hearst <derekallenhearst@gmail.com>
2023-02-20 12:28:27 -0700

    try out defer load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,20 @@
 import type { PageServerLoad } from './$types'
 import { prisma } from '$lib/prisma'
+export const ssr = false
+
 export const load = (async () => {
 	return {
-		keeps: prisma.keep.findMany({
-			include: {
-				user: true,
-				_count: {
-					select: {
-						vaults: true
+		defer: {
+			keeps: prisma.keep.findMany({
+				include: {
+					user: true,
+					_count: {
+						select: {
+							vaults: true
+						}
 					}
 				}
-			}
-		})
+			})
+		}
 	}
 }) satisfies PageServerLoad
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,21 @@
 	import Keep from '$lib/Keep.svelte'
 	import { navigating } from '$app/stores'
 	export let data: PageData
+
 </script>
 
 <svelte:head>
 	<title>Keepr</title>
 </svelte:head>
 
-{#if data.keeps.length > 0}
+{#await data.defer.keeps}
+	<div class="origin-center animate-spin text-center">
+		<i class="mdi mdi-loading text-8xl  text-black" />
+	</div>
+{:then keeps}
 	<div class="columns-2 gap-8 p-3 md:columns-4">
-		{#each data.keeps as keep (keep.id)}
+		{#each keeps as keep (keep.id)}
 			<Keep {keep} />
 		{/each}
 	</div>
-{/if}
+{/await}
